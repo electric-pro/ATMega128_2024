e0ed820113c57340a5fd6f120cbf9b4ae084ed2a
Deahyeon Yu <electric-pro@github.com>
2024-10-02 15:41:57 +0900

    1

---
+++ Test01/Test01/main.c
@@ -0,0 +1,18 @@
+/*
+ * Test01.c
+ *
+ * Created: 10/2/2024 12:40:42 PM
+ * Author : dbeog
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
